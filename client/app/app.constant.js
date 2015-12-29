77be71c5916f762eb391764c3d5dbf6416778ed7
Albert <kovalevskij.albert@gmail.com>
2015-12-29 19:57:59 +0200

    add missing files

---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('stproxyServiceApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
