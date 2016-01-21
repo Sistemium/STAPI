25619ed065e24cfdf28dd07fcc441965b6ead186
Alexander Levin <alevin.ru@gmail.com>
2016-01-21 16:34:56 +0200

    validateParams fix

--- server/middleware/validateParams.js
+++ server/middleware/validateParams.js
@@ -6,7 +6,7 @@ export default function () {
 
     _.each(req['x-params'], (param, key) => {
       if (key) {
-        if (key.match(/[\[\]-_\s]/i)) {
+        if (key.match(/[^-_$@a-z0-9 :]/i)) {
           console.log(`Invalid key in params: ${key}`);
           next(new Error(`Param value cannot contain such symbols: "[", "]", "-", "_", "space"`));
         }
