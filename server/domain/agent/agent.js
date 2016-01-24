1d9d2653d0a95e72f4912ff7643525a9bbfe64b6
Alexander Levin <alevin.ru@gmail.com>
2016-01-24 16:18:10 +0200

    simplify agent config

--- server/domain/agent/agent.js
+++ server/domain/agent/agent.js
@@ -1,27 +1,31 @@
 module.exports = {
+
   pools: ['phatest'],
   extends: 'defaultFields',
+  tableName: '[pha].[Agent]',
+  alias: 'Agent',
+  collection: 'agent',
+
   fields: {
-    code: {
-      field: 'id'
-    },
-    name: 'name',
+    code: 'id',
+    name: true,
     mobileNumber: {
       expr: 'substring(mobile_number,2)',
-      field: 'mobile_number'
+      field: 'mobile_number',
+      converter: function (mobileNumber) {
+        return mobileNumber ? '8' + mobileNumber.replace(/(8|^)([0-9]{10,11}).*$/,'$2') : null;
+      }
     },
-    org: 'org',
-    info: 'info',
-    email: 'email',
-    roles: 'roles',
+    org: true,
+    info: true,
+    email: true,
+    roles: true,
     isDisabled: {
       type: 'boolean'
     },
     lastAuth: {
       expr: '(select max([lastAuth]) from [pha].[accesstoken] where [agent] = [Agent].[id])'
     }
-  },
-  tableName: '[pha].[Agent]',
-  alias: 'Agent',
-  collection: 'agent'
+  }
+
 };
