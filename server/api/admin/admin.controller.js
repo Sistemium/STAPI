c66d99fe29f7ce93cfe0a6b5af475896a5ddfe4d
Albert <kovalevskij.albert@gmail.com>
2016-01-25 13:58:52 +0200

    get single config from query, params not working yet

--- server/api/admin/admin.controller.js
+++ server/api/admin/admin.controller.js
@@ -1,17 +1,24 @@
 'use strict';
 import domainConfig from '../../config/domainConfig';
-
-function mapToJson(map) {
-  return JSON.parse(JSON.stringify([...map]));
-}
+import path from 'path';
 
 export function index(req, res) {
-  return res.json(mapToJson(req.app.locals.domainConfig));
-}
+  let collection = req.params.collection || req.query.collection;
+  if (collection) {
+    let config = req.app.locals.domainConfig.get(collection);
+    if (config) {
+      return res.json(config);
+    } else {
+      return res.status(404).end('Config not found...');
+    }
+  }
 
-export function reload(req, res) {
-  domainConfig('../../domain', map => {
-    req.app.locals.domainConfig = map;
-    return res.json(mapToJson(map));
-  });
+  if (req.query && req.query.reload) {
+    domainConfig(path.join(__dirname, '../..', 'domain'), map => {
+      req.app.locals.domainConfig = map;
+      return res.json([...map]);
+    });
+  } else {
+    return res.json([...req.app.locals.domainConfig]);
+  }
 }
