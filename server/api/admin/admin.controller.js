96a1b45b067068ad3fa8d11159296d90cfca7cfb
Albert <kovalevskij.albert@gmail.com>
2016-01-25 12:52:36 +0200

    reload config

--- server/api/admin/admin.controller.js
+++ server/api/admin/admin.controller.js
@@ -1,5 +1,17 @@
 'use strict';
+import domainConfig from '../../config/domainConfig';
+
+function mapToJson(map) {
+  return JSON.parse(JSON.stringify([...map]));
+}
 
 export function index(req, res) {
-  return res.json(req.app.locals);
+  return res.json(mapToJson(req.app.locals.domainConfig));
+}
+
+export function reload(req, res) {
+  domainConfig('../../domain', map => {
+    req.app.locals.domainConfig = map;
+    return res.json(mapToJson(map));
+  });
 }
