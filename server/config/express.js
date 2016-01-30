ba77547da6292d5f4f5d415f1582aa6fba245b50
Alexander Levin <alevin.ru@gmail.com>
2016-01-30 12:44:57 +0200

    process.env.MORGAN

--- server/config/express.js
+++ server/config/express.js
@@ -32,7 +32,7 @@ export default function (app) {
   if ('production' === env) {
     //app.use(favicon(path.join(config.root, 'client', 'favicon.ico')));
     app.use(express.static(app.get('appPath')));
-    app.use(morgan('dev'));
+    app.use(morgan(process.env.MORGAN || 'dev'));
   }
 
   if ('development' === env) {
@@ -44,7 +44,7 @@ export default function (app) {
   if ('development' === env || 'test' === env) {
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(app.get('appPath')));
-    app.use(morgan('dev'));
+    app.use(morgan(process.env.MORGAN || 'dev'));
     app.use(errorHandler()); // Error handler - has to be last
   }
 }
