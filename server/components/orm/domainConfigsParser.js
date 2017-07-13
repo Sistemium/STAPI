7bd64050073ca54bc27e4181a93f48c9321fa78d
Alexander Levin <alevin.ru@gmail.com>
2017-07-13 11:42:56 +0300

    domainConfigsParser formatting

--- server/components/orm/domainConfigsParser.js
+++ server/components/orm/domainConfigsParser.js
@@ -1,12 +1,38 @@
 'use strict';
 
-const _ = require('lodash');
-import plugins from '../plugins/index';
-const dir = require('node-dir');
+export default domainConfigParser;
+
+import _ from 'lodash';
+import dir from 'node-dir';
+
 const debug = require('debug')('stapi:domainConfig');
-import {getPoolsKeys,getPoolByName} from '../pool';
+
+import plugins from '../plugins/index';
+import {getPoolsKeys, getPoolByName} from '../pool';
 
 let map = new Map();
+
+
+function domainConfigParser(path, callback) {
+
+  readConfigFiles(path, (files) => {
+
+    _.each(files, (file) => {
+
+      let config = require(file);
+      let filename = file.split('/').slice(-1)[0].match(/([^\/]+)(?=\.\w+$)/)[0];
+
+      processConfig(config, filename);
+
+    });
+
+    addRefsToConfigs(map);
+    callback(map);
+
+  });
+
+}
+
 
 function parseFields(fields) {
 
@@ -15,62 +41,66 @@ function parseFields(fields) {
   if (!_.isObject(fields)) throw new Error('Model definition must be an object');
 
   _.each(fields, (field, key) => {
-    if (_.isObject(field)) {
 
-      if (field.expr && !field.converter && field.readonly !== false) {
-        field.readonly = true;
-      }
-
-      if (field.parser) {
-        if (!(typeof field.parser === 'function')) {
-          field.parser = plugins().get(field.parser);
-        }
-      } else {
-        let typeParser = plugins().get('parse.' + field.type);
-        if (typeParser) {
-          field.parser = typeParser;
-        }
-      }
-
-      if (field.converter) {
-        if (!(typeof field.converter === 'function')) {
-          field.converter = plugins().get(field.converter);
-        }
-      } else {
-        let typeConverter = plugins().get('convert.' + field.type);
-        if (typeConverter) {
-          field.converter = typeConverter;
-        }
-      }
-
-      if (field.validator) {
-        if (!(typeof field.validator === 'function')) {
-          field.validator = plugins().get(field.validator);
-        }
-      } else {
-        let typeValidator = plugins().get('validator.' + field.type);
-        if (typeValidator) {
-          field.validator = typeValidator;
-        }
-      }
-
-      if (field.fields) {
-        field.fields = parseFields(field.fields);
-        //debug (key, field.fields);
-      }
-
-      parsed[key] = field;
-
-    } else {
+    if (!_.isObject(field)) {
       throw new Error('Invalid model definition');
     }
+
+    if (field.expr && !field.converter && field.readonly !== false) {
+      field.readonly = true;
+    }
+
+    if (field.parser) {
+      if (!(typeof field.parser === 'function')) {
+        field.parser = plugins().get(field.parser);
+      }
+    } else {
+      let typeParser = plugins().get('parse.' + field.type);
+      if (typeParser) {
+        field.parser = typeParser;
+      }
+    }
+
+    if (field.converter) {
+      if (!(typeof field.converter === 'function')) {
+        field.converter = plugins().get(field.converter);
+      }
+    } else {
+      let typeConverter = plugins().get('convert.' + field.type);
+      if (typeConverter) {
+        field.converter = typeConverter;
+      }
+    }
+
+    if (field.validator) {
+      if (!(typeof field.validator === 'function')) {
+        field.validator = plugins().get(field.validator);
+      }
+    } else {
+      let typeValidator = plugins().get('validator.' + field.type);
+      if (typeValidator) {
+        field.validator = typeValidator;
+      }
+    }
+
+    if (field.fields) {
+      field.fields = parseFields(field.fields);
+      //debug (key, field.fields);
+    }
+
+    parsed[key] = field;
+
   });
 
   return parsed;
+
 }
 
-let normalizeConfig = (cfg, filename) => {
-  if (!cfg || !_.isObject(cfg)) throw new Error(`Config not passed...`);
+function normalizeConfig(cfg, filename) {
+
+  if (!cfg || !_.isObject(cfg)) {
+    throw new Error(`Config not passed...`);
+  }
 
   let nCfg = _.cloneDeep(cfg);
 
@@ -97,7 +127,7 @@ let normalizeConfig = (cfg, filename) => {
       nCfg.fields[key].field = key;
     }
 
-    _.each(nCfg.fields[key].fields, (subField, subKey)=> {
+    _.each(nCfg.fields[key].fields, (subField, subKey) => {
       nCfg.fields[key].fields[subKey] = _.assign({
         field: subKey
       }, _.isString(subField) ? {field: subField} : subField);
@@ -113,97 +143,121 @@ let normalizeConfig = (cfg, filename) => {
   });
 
   return nCfg;
-};
 
-let processConfig = (cfg, filename) => {
+}
+
+function processConfig(cfg, filename) {
 
   let extendedCfg = normalizeConfig(cfg, filename);
+
   let pools = cfg.pools && _.filter(getPoolsKeys(), key => {
-      let aliases = getPoolByName(key).config.aliasesRe;
-      return key.match('^(' + cfg.pools.join('|') + ')$') || aliases && _.find(cfg.pools, function (key) {
-          return key.match(aliases)
-        });
+
+      let {aliasesRe} = getPoolByName(key).config;
+
+      return key.match(`^(${cfg.pools.join('|')})$`) ||
+        aliasesRe && _.find(cfg.pools, key => key.match(aliasesRe));
+
     });
 
-  _.forEach(pools || [''], function (pool) {
-    var extendsArray = Array.isArray(cfg.extends) ? cfg.extends : [cfg.extends];
+  _.forEach(pools || [''], pool => {
+
+    let extendsArray = Array.isArray(cfg.extends) ? cfg.extends : [cfg.extends];
     let pooledCfg = _.cloneDeep(extendedCfg);
-    _.forEach(extendsArray, function (parent) {
+
+    _.forEach(extendsArray, parent => {
 
       let parentCfg = {};
+
       if (parent) {
+
         parent = parent.toLowerCase();
         parentCfg = _.cloneDeep(map.get(pool + '/' + parent) || map.get('/' + parent));
+
         if (!parentCfg) {
           throw new Error('Unknown pool:' + pool);
         }
+
         delete parentCfg.abstract;
         delete parentCfg.pools;
+
       }
+
       pooledCfg = _.merge(parentCfg, extendedCfg);
       delete pooledCfg.pools;
+
     });
+
     pooledCfg.pool = pool;
+
     let mapKey = pool + '/' + pooledCfg.collection.toLowerCase();
+
     map.set(mapKey, pooledCfg);
+
   });
-};
+
+}
 
 function addRefsToConfigs(map) {
 
-  map.forEach((config) => {
+  map.forEach(config => {
 
     let pool = config.pool;
 
-    if (!config.abstract) {
-      _.each(config.fields, (val, key) => {
-        if (val.ref) {
-          let refConfig = map.get(`${pool}/${val['ref'].toLowerCase()}`);
-          if (!refConfig) {
-            console.error ('no ref config: ', val);
-          }
-          _.assign(config.fields[key], {
-            //alias: refConfig.alias,
-            tableName: refConfig.tableName,
-            //TODO: ref id hardcoded for now, change it that id will have some special property
-            id: refConfig.fields.id.field,
-            refConfig: refConfig
-          });
-
-          if (!val.alias) {
-            val.alias = key;
-            if (config.refAliasRe) {
-              let matched = key.match (config.refAliasRe);
-              if (matched) {
-                val.alias = _.last (matched);
-              }
-            }
-          }
-
-
-          map.set(`${pool}/${config.collection.toLowerCase()}`, config);
-        }
-      });
+    if (config.abstract) {
+      return;
     }
-  });
-}
 
-export default function (path, cb) {
-  readConfigFiles(path, (files) => {
-    _.each(files, (file) => {
-      let cnfg = require(file);
-      let filename = file.split('/').slice(-1)[0].match(/([^\/]+)(?=\.\w+$)/)[0];
-      processConfig(cnfg, filename);
+    _.each(config.fields, (val, key) => {
+
+      if (!val.ref) {
+        return;
+      }
+
+      let refConfig = map.get(`${pool}/${val['ref'].toLowerCase()}`);
+
+      if (!refConfig) {
+        console.error('no ref config: ', val);
+      }
+
+      _.assign(config.fields[key], {
+
+        //alias: refConfig.alias,
+        tableName: refConfig.tableName,
+        //TODO: ref id hardcoded for now, change it that id will have some special property
+        id: refConfig.fields.id.field,
+        refConfig: refConfig
+
+      });
+
+      if (!val.alias) {
+
+        val.alias = key;
+
+        if (config.refAliasRe) {
+          let matched = key.match(config.refAliasRe);
+          if (matched) {
+            val.alias = _.last(matched);
+          }
+        }
+
+      }
+
+      map.set(`${pool}/${config.collection.toLowerCase()}`, config);
+
     });
 
-    addRefsToConfigs(map);
-    cb(map);
   });
-};
 
-let readConfigFiles = (path, cb) => {
+}
+
+function readConfigFiles(path, cb) {
+
   dir.files(path, (err, files) => {
-    if (err) throw err;
+
+    if (err) {
+      throw err;
+    }
+
     files.sort((a, b) => {
       let aSlashNumber = a.match(/\//ig);
       let bSlashNumber = b.match(/\//ig);
@@ -215,5 +269,7 @@ let readConfigFiles = (path, cb) => {
     });
 
     cb(files);
+
   });
-};
+
+}
