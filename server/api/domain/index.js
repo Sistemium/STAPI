f1289f1a4de83b49ed87e2d586343042515718ab
Alexander Levin <alevin.ru@gmail.com>
2018-01-16 10:02:35 +0200

    cacheControl

--- server/api/domain/index.js
+++ server/api/domain/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var express = require('express');
-var controller = require('./domain.controller.js');
+import express from 'express';
 import headersToParams from '../../middleware/headersToParams';
 import validateParams from '../../middleware/validateParams';
 import extractFromUrl from '../../middleware/extractFromUrl';
@@ -11,22 +10,40 @@ import modelPredicates from '../../middleware/modelPredicates';
 import modelConfig from '../../middleware/modelConfig';
 import auth from '../../middleware/auth';
 
-var router = express.Router();
-var mw = [extractFromUrl(), auth('auth'), headersToParams(), validateParams(), modelConfig('join'), modelPredicates()];
-var bm = [modifyBody(), validateModel(), controller.post];
+const controller = require('./domain.controller.js');
+
+const router = express.Router();
+const mw = [
+  extractFromUrl(), auth('auth'),
+  headersToParams(), validateParams(), modelConfig('join'), modelPredicates(),
+  cacheControl
+];
+const bm = [modifyBody(), validateModel(), controller.post];
 
 router
-  .get ('/:collection/:id?', mw, controller.index)
+  .get('/:collection/:id?', mw, controller.index)
   .post('/:collection/:id?', mw, bm)
-  .put ('/:collection/:id?', mw, bm)
+  .put('/:collection/:id?', mw, bm)
   .patch('/:collection/:id?', mw, bm)
   .delete('/:collection/:id', mw, controller.del)
 
-  .get ('/:filterCollection/:filterCollectionId/:collection/:id?', mw, controller.index)
+  .get('/:filterCollection/:filterCollectionId/:collection/:id?', mw, controller.index)
   .post('/:filterCollection/:filterCollectionId/:collection/:id?', mw, bm)
-  .put ('/:filterCollection/:filterCollectionId/:collection/:id?', mw, bm)
+  .put('/:filterCollection/:filterCollectionId/:collection/:id?', mw, bm)
   .patch('/:filterCollection/:filterCollectionId/:collection/:id?', mw, bm)
   .delete('/:filterCollection/:filterCollectionId/:collection/:id?', mw, controller.del)
 ;
 
 module.exports = router;
+
+function cacheControl(req, res, next) {
+
+  let {config} = res.locals;
+
+  if (config && config.caching === false) {
+    res.header('Cache-Control', 'no-cache');
+  }
+
+  next();
+
+}
