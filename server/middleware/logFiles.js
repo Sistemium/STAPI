f72c16c79d07234e576ffab16a834a08a18df39e
Alexander Levin <alevin.ru@gmail.com>
2018-06-11 00:04:13 +0300

    LOG_SUCCESS_REQUEST_FOLDER

--- server/middleware/logFiles.js
+++ server/middleware/logFiles.js
@@ -1,34 +1,64 @@
-export {logErrorRequestToFile};
+export {logErrorRequestToFile, logSuccessRequestToFile};
 
 import fs from 'fs';
 import _ from 'lodash';
-const uuidV4 = require('uuid').v4;
+import {getPoolByName} from '../components/pool';
 
-const folder = process.env.LOG_ERROR_REQUEST_FOLDER;
+const uuidV4 = require('uuid').v4;
+const debug = require('debug')('stapi:logRequestToFile');
+
+const errorFolder = process.env.LOG_ERROR_REQUEST_FOLDER;
+const successFolder = process.env.LOG_SUCCESS_REQUEST_FOLDER;
 
 function logErrorRequestToFile(err, req, res, next) {
 
-  if (!err) {
-    return next();
+  debug('logErrorRequestToFile:', errorFolder);
+
+  writeToFile(errorFolder, err.toString(), req);
+
+  next({ text: err.message });
+
+}
+
+function logSuccessRequestToFile(req, res, next) {
+
+  const { body } = req;
+
+  if (!body.length && !Object.keys(body).length) {
+    return next()
   }
 
-  if (folder) {
+  debug('logSuccessRequestToFile:', req.pool);
 
-    let prefix = _.first(req.originalUrl.match(/[^/]+\/[^/]+$/)) || 'undefined';
+  try {
 
-    let fileName = `${folder}/${prefix.replace(/[^a-z0-9]/ig, '-')}-${uuidV4()}.json`;
+    let { config: { logToFolder } } = getPoolByName(req.pool);
 
-    fs.writeFile(fileName, err.body, err => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`Request written to "${fileName}"`);
-      }
-    });
+    if (logToFolder) {
+      debug('logSuccessRequestToFile:', successFolder, logToFolder);
+      writeToFile(`${successFolder}/${logToFolder}`, body, req);
+    }
 
+  } catch (e) {
+    next(e)
   }
 
-  console.error(err.message, req.headers);
-  next({text: err.message});
+  next();
+
+}
+
+function writeToFile(folder, json, req) {
+
+  let prefix = _.first(req.originalUrl.match(/[^/]+\/[^/]+$/)) || 'undefined';
+
+  let fileName = `${folder}/${prefix.replace(/[^a-z0-9]/ig, '-')}-${uuidV4()}.json`;
+
+  fs.writeFile(fileName, JSON.stringify(json), err => {
+    if (err) {
+      console.error(err);
+    } else {
+      debug('written to:', fileName);
+    }
+  });
 
 }
--- server/routes.js
+++ server/routes.js
@@ -7,12 +7,21 @@
 import errors from './components/errors';
 import auth from './middleware/auth';
 import extractPool from './middleware/extractPool';
+import {logSuccessRequestToFile} from "./middleware/logFiles";
 
 export default function (app) {
-  // Insert routes below
+
+  const pool = [extractPool(), auth('middleware')];
+
+  if (process.env.LOG_SUCCESS_REQUEST_FOLDER) {
+    pool.push(logSuccessRequestToFile);
+  }
+
+  pool.push(require('./api/domain'));
+
   app.use('/api/sts', require('./api/sts'));
   app.use('/api/admin/map', require('./api/admin'));
-  app.use('/api/:pool', extractPool(), auth('middleware'), require('./api/domain'));
+  app.use('/api/:pool', pool);
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(auth|components|app|bower_components|assets)/*')
